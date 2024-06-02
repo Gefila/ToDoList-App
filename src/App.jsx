d1997e688a075423540243bea14f16b1e497f76b
Gefila <106391467+Gefila@users.noreply.github.com>
2024-06-02 22:50:03 +0700

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,30 @@ function App() {
 
 	return (
 		<div className="w-full h-screen bg-indigo-950 p-5 text-white flex justify-center items-start">
-			<div className="flex justify-center flex-col items-center bg-indigo-900 p-3 w-full xl:w-1/2">
-				<div className="py-3 bg-indigo-800 w-full text-center">
+			<div className="flex justify-center flex-col items-center bg-indigo-900 p-3 w-full xl:w-1/2 rounded-lg">
+				<div className="py-3 bg-indigo-700 w-full text-center rounded-t-md">
 					<h1 className="text-white">ToDoList App</h1>
 				</div>
-				<button
-					className="bg-indigo-400 px-4 py-2 w-full"
-					onClick={() => setShowModal(true)}
-				>
-					Add Task
-				</button>
+				<div className="flex w-full">
+					<button
+						className="bg-indigo-400 px-4 py-2 rounded-bl-md w-9/12 hover:bg-indigo-500"
+						onClick={() => setShowModal(true)}
+					>
+						Add Task
+					</button>
+					<select
+						name=""
+						id=""
+						className="text-black px-3 py-2 rounded-br-md w-1/4"
+					>
+						<option value="1" selected disabled>
+							Filter Task
+						</option>
+						<option value="low">Low</option>
+						<option value="medium">Medium</option>
+						<option value="high">High</option>
+					</select>
+				</div>
 				{showModal && (
 					<div className="fixed backdrop-blur-sm top-0 left-0 right-0 bottom-0 flex items-center justify-center">
 						<div
@@ -32,7 +46,9 @@ function App() {
 								closeModal ? "scale-in-center" : ""
 							} ${isClosing ? "scale-out-center" : ""}`}
 						>
-							<h1 className="text-center py-3 text-lg font-bold">Masukkan TODO</h1>
+							<h1 className="text-center py-3 text-lg font-bold">
+								Masukkan TODO
+							</h1>
 							<form className="px-5 pb-5 flex flex-col gap-3">
 								<div className="flex flex-col">
 									<label htmlFor="" className="text-xl">
@@ -41,7 +57,7 @@ function App() {
 									<input
 										type="text"
 										placeholder="Masukkan Task"
-										className="px-3 py-2 rounded-md text-black"
+										className="input"
 									/>
 								</div>
 								<div className="flex flex-col">
@@ -51,7 +67,7 @@ function App() {
 									<input
 										type="text"
 										placeholder="Masukkan Deskripsi"
-										className="px-3 py-2 rounded-md text-black"
+										className="input"
 									/>
 								</div>
 								<div className="flex flex-col">
@@ -61,15 +77,17 @@ function App() {
 									<input
 										type="datetime-local"
 										placeholder="Masukkan Deskripsi"
-										className="px-3 py-2 rounded-md text-black"
+										className="input"
 									/>
 								</div>
 								<div className="flex flex-col">
 									<label htmlFor="" className="text-xl">
 										Task Priority
 									</label>
-									<select name="" id="" className="text-black px-3 py-2 rounded-md">
-										<option value="1" selected disabled>Select Task Priority</option>
+									<select name="" id="" className="input">
+										<option value="1" selected disabled>
+											Select Task Priority
+										</option>
 										<option value="low">Low</option>
 										<option value="medium">Medium</option>
 										<option value="high">High</option>
@@ -77,13 +95,13 @@ function App() {
 								</div>
 								<div className="flex mt-5 gap-4">
 									<button
-										className="bg-indigo-400 px-4 py-2 w-full rounded-md"
+										className="bg-indigo-400 px-4 py-2 w-full rounded-md hover:bg-indigo-200 hover:text-blue-950"
 										onClick={closeModal}
 										type="button"
 									>
 										Cancel
 									</button>
-									<button className="bg-indigo-700 px-4 py-2 w-full rounded-md">
+									<button className="bg-indigo-700 px-4 py-2 w-full rounded-md hover:bg-indigo-800">
 										Add Task
 									</button>
 								</div>
@@ -92,7 +110,6 @@ function App() {
 					</div>
 				)}
 
-				<div>Task</div>
 			</div>
 		</div>
 	);
